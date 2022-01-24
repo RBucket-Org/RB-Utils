e02e4e8d709ec871ffcb38503ecc2beca0aaadeb
egnimos <egnimos25@gmail.com>
2022-01-24 22:58:44 +0530

    validate middleware

--- utils/middleware/validate_middleware.go
+++ utils/middleware/validate_middleware.go
@@ -18,28 +18,28 @@ func (mv *MiddlewareValue) GetMiddlewareValue(ctx *gin.Context) rest_errors.Rest
 	//if the request is from public api then bypass the validation
 	middleWareType, check := ctx.Get("middleware_type")
 	if !check {
-		err := rest_errors.NewBadRequestError("token is invalid")
+		err := rest_errors.NewBadRequestError("token is invalid because the middleware type is not valid")
 		return err
 	}
 	//get the user_id from the middleware
 	userID, check := ctx.Get("user_id")
 	if !check {
-		err := rest_errors.NewBadRequestError("token is invalid")
+		err := rest_errors.NewBadRequestError("token is invalid because the user id is not valid")
 		return err
 	}
 	//get the device_id from the middleware
 	deviceID, check := ctx.Get("device_id")
 	if !check {
-		err := rest_errors.NewBadRequestError("token is invalid")
+		err := rest_errors.NewBadRequestError("token is invalid because the device id is not valid")
 		return err
 	}
 	//get the identity_key from the middleware
 	identityKey, check := ctx.Get("identity_key")
 	if !check {
-		err := rest_errors.NewBadRequestError("token is invalid")
+		err := rest_errors.NewBadRequestError("token is invalid because the identity key is not valid")
 		return err
 	}
-	fmt.Println("userID, deviceID, identityKey", userID, deviceID, identityKey)
+	fmt.Printf("userID:%s\n, deviceID:%s\n, identityKey:%s\n, type:%s\n", userID, deviceID, identityKey, middleWareType)
 
 	mv.UserID = userID.(int64)
 	mv.DeviceID = deviceID.(string)
